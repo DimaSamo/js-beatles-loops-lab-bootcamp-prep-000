c62a094165e09e0e47a9d813438975314ca334c7
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-13 23:20:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+  for (var i=0; i<musicians.length; i++){
+    array[i]= `${musicians[i]} plays ${instruments[i]}`
+  }
+  return array
+}
+
+function johnLennonFacts(array){
+  var i=0
+  while (i<array.length){
+    array[i]=array[i]+"!!!"
+    i++
+  }
+}
